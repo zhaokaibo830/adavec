29b448d0da2def48ff82308540780998b16d7c04
zhaokaibo830 <73161415+zhaokaibo830@users.noreply.github.com>
2025-04-23 11:04:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/adavec',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
